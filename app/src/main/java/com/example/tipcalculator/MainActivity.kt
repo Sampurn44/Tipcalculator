72cf795222b51f31f4b58a6b8fcac09f533e6958
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2023-04-23 05:33:55 +0530

    Keyboard Actions Updated

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -24,8 +25,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,7 +61,7 @@ fun TipTimeScreen(){
     var tipInput by remember { mutableStateOf("") }
     val tipPercentage= tipInput.toDoubleOrNull()?:0.0
     val tip = CalculateTip(amount,tipPercentage)
-
+    val focusManager = LocalFocusManager.current
     Column(modifier = Modifier.padding(32.dp), verticalArrangement = Arrangement.spacedBy(8.dp) ) {
         Text(
             text = stringResource(id = R.string.calculate_tip),
@@ -65,9 +69,19 @@ fun TipTimeScreen(){
             modifier= Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumber(label=R.string.bill_amount, value=amountInput, onValueChange = {amountInput=it})
+        EditNumber(label=R.string.bill_amount,
+            value=amountInput,
+            onValueChange = {amountInput=it},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
+            keyboardActions= KeyboardActions(onNext = {focusManager.moveFocus(FocusDirection.Down)})
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumber(label=R.string.how_service, value=tipInput, onValueChange = {tipInput=it})
+        EditNumber(label=R.string.how_service,
+            value=tipInput,
+            onValueChange = {tipInput=it},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {focusManager.clearFocus()})
+        )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
             text = stringResource(id=R.string.tip_amount,tip),
@@ -80,19 +94,26 @@ fun TipTimeScreen(){
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditNumber(@StringRes label: Int, value: String, onValueChange:(String)-> Unit,modifier: Modifier=Modifier ){
+fun EditNumber(@StringRes label: Int,
+               value: String, onValueChange:(String)-> Unit,
+               keyboardOptions: KeyboardOptions,
+               keyboardActions: KeyboardActions,
+               modifier: Modifier=Modifier ){
 
     TextField(
         value = value,
         onValueChange =onValueChange,
-        label = { Text(
+        label = {
+            Text(
             text = stringResource(id = label),
-            modifier = Modifier.fillMaxWidth() )},
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth() )
+                },
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
         singleLine = true
     )
 }
-private fun CalculateTip(Amount:Double,tipPercentage : Double=15.0): String
+private fun CalculateTip(Amount:Double,tipPercentage : Double): String
 {
 val tip = (tipPercentage/100) * Amount
    return NumberFormat.getCurrencyInstance().format(tip)
