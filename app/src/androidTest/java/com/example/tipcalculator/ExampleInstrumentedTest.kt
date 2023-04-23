4a454fb060eeecc63fbcedd90b4b0b5ffb4b7d39
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2023-04-23 17:46:18 +0530

    "Not a Feature"-> Instrumentaion Test added

--- app/src/androidTest/java/com/example/tipcalculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tipcalculator/ExampleInstrumentedTest.kt
@@ -1,24 +1,41 @@
 package com.example.tipcalculator
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.example.tipcalculator.ui.theme.TipcalculatorTheme
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-
+import java.text.NumberFormat
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tipcalculator", appContext.packageName)
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipcalculatorTheme {
+                Surface (modifier = Modifier.fillMaxSize()){
+                    TipTimeScreen()
+                }
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+        )
     }
 }
